02548cb7b9cac7fa78a10a0dccf5908bd551397c
jorropowathil <85700575+jorropowathil@users.noreply.github.com>
2021-06-24 20:30:58 -0400

    Added random character functions

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,38 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+//functions (from https://www.w3schools.com/html/html_charset.asp)
+function randomLower () {
+  return String.fromCharCode(Math.floor(Math.random ()* 26) + 97);
 }
+console.log (randomLower())
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+function randomUpper (){
+  return String.fromCharCode(Math.floor(Math.random()* 26) + 65);
+}
+console.log (randomUpper())
+
+function randomSymbol (){
+  return String.fromCharCode(Math.floor(Math.random()*15) + 33)
+}
+console.log (randomSymbol())
+
+function randomNumber () {
+  return String.fromCharCode(Math.floor(Math.random()*10) + 48)
+}
+console.log (randomNumber())
+
+// // Letters
+// letters=
+// // Assignment Code
+// var generateBtn = document.querySelector("#generate");
+
+// // Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+
+
+//   passwordText.value = password;
+
+// }
+
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
