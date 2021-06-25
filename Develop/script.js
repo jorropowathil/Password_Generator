7b6532121f66479a4904ffc639ca0f043c9b6035
jorropowathil <85700575+jorropowathil@users.noreply.github.com>
2021-06-24 22:26:17 -0400

    Continued work on password generator function. Added if else

--- Develop/script.js
+++ Develop/script.js
@@ -12,11 +12,20 @@ function randomUpper (){
 function randomSymbol (){
   return String.fromCharCode(Math.floor(Math.random()*15) + 33)
 }
-console.log (randomSymbol())
+// console.log (randomSymbol())
 
 function randomNumber () {
   return String.fromCharCode(Math.floor(Math.random()*10) + 48)
 }
+// console.log (randomNumber())
+
+var randomFunction = {
+  lower: randomLower, 
+  upper: randomUpper, 
+  symbol: randomSymbol, 
+  number: randomNumber
+};
+
 
 function generatePassword (){
   var submitLength = parseInt(prompt("How long would you like your password (between 10-20)?"));
@@ -30,15 +39,60 @@ function generatePassword (){
     submitLength;
   }
   else {
-    wantLower = window.confirm ("Do you want lower case characters?");
-    wantUpper = window.confirm ("Do you want upper case characters?");
-    wantSymbol = window.confirm ("Do you want symbol characters?");
-    wantNumber = window.confirm ("Do you want numbers?");
+    var wantLower = window.confirm ("Do you want lower case characters?");
+    var wantUpper = window.confirm ("Do you want upper case characters?");
+    var wantSymbol = window.confirm ("Do you want symbol characters?");
+    var wantNumber = window.confirm ("Do you want numbers?");
   }
-}
+  // Possible situtations:
+  // All denied 
+  if (!wantLower && !wantUpper && !wantSymbol && !wantNumber){
+    alert("You have to pick one...");
+    var wantLower = window.confirm ("Do you want lower case characters?");
+    var wantUpper = window.confirm ("Do you want upper case characters?");
+    var wantSymbol = window.confirm ("Do you want symbol characters?");
+    var wantNumber = window.confirm ("Do you want numbers?");
+  }
+    // 4 accepted
+    else if (wantLower && wantUpper && wantSymbol && wantNumber){
+      for (var i = 0; i < zooAnimals.length; i++) { 
+        console.log("I am going to zoo to see " + zooAnimals[i] + ".");
+    }
+    }
+    // 3 accepted 1 denied 
+    else if (wantLower && wantUpper && wantSymbol && !wantNumber){
+    }
+    else if (wantLower && wantUpper && !wantSymbol && wantNumber){
+    }
+    else if (wantLower && !wantUpper && wantSymbol && wantNumber){
+    }
+    else if (!wantLower && wantUpper && wantSymbol && wantNumber){
+    }
+    // 2 accepted, 2 denied 
+    else if (wantLower && wantUpper && !wantSymbol && !wantNumber){
+    }
+    else if (wantLower && !wantUpper && wantSymbol && !wantNumber){
+    }
+    else if (!wantLower && wantUpper && wantSymbol && !wantNumber){
+    }
+    else if (!wantLower && !wantUpper && wantSymbol && wantNumber){
+    }
+    else if (!wantLower && wantUpper && !wantSymbol && wantNumber){
+    }
+    else if (wantLower && !wantUpper && !wantSymbol && wantNumber){
+    }
+    // 1 accepted, 3 denied
+    else if (!wantLower && wantUpper && wantSymbol && wantNumber){
+    }
+    else if (wantLower && !wantUpper && wantSymbol && wantNumber){
+    }
+    else if (wantLower && wantUpper && !wantSymbol && wantNumber){
+    }
+    else if (wantLower && wantUpper && wantSymbol && !wantNumber){
+    }
+  }
 
-console.log (generatePassword)
-// console.log (randomNumber())
+
 
 
 // // Letters
@@ -50,8 +104,6 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  window.prompt ("Do you want lower case letters?");
-
 
   passwordText.value = password;
 
